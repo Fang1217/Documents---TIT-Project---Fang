cb6d50f8e7b6f375ea699fa042f97edb2663e335
Sleepin'_Fang <57334019+Fang1217@users.noreply.github.com>
2024-05-05 20:30:33 +0800

    Working program

---
+++ huffman_coding.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import math
+
+class Node:
+    def __init__(self, symbol, probability, left=None, right=None):
+        self.symbol = symbol
+        self.probability = probability
+        self.left = left
+        self.right = right
+
+    def __lt__(self, other):
+        return self.probability > other.probability
+
+def huffman_coding(text):
+    # Return encoding table DataFrame
+    text = text.upper()
+    letter_counts = {}
+    total_letters = 0
+
+    for letter in text:
+        if letter.isalpha():
+            letter_counts[letter] = letter_counts.get(letter, 0) + 1
+            total_letters += 1
+
+    if total_letters == 0:
+        return pd.DataFrame() # return empty dataframe as there are no letters
+    
+    unique_letters = list(letter_counts.keys())
+    probabilities = [letter_counts[letter] / total_letters for letter in unique_letters]
+
+    nodes = [Node(symbol, prob) for symbol, prob in zip(unique_letters, probabilities)]
+
+    while len(nodes) > 1:
+        nodes.sort()
+        right, left = nodes.pop(), nodes.pop()
+        parent = Node(None, left.probability + right.probability, left, right)
+        
+        # insert combined node in front such that the combined probability will
+        # have higher precedence if there exist other symbols with equal probability.
+        nodes.insert(0, parent) 
+
+    codes = {}
+    curr_node = nodes[0]
+    curr_code = ""
+
+    def traverse(node, code):
+        if node is None:
+            return
+        if node.symbol is not None:
+            codes[node.symbol] = code
+            return
+
+        traverse(node.left, code + "0")  # Second last 
+        traverse(node.right, code + "1") # Last
+
+    traverse(curr_node, curr_code)
+    
+    data = {
+        'Letter': unique_letters
+    }
+    df = pd.DataFrame(data)
+    df['Codeword'] = [codes[letter] for letter in unique_letters]
+    df['Probability'] = [round(probability, 2) for probability in probabilities]
+    df['Length'] = [len(codes[letter]) for letter in unique_letters]
+
+    return df
+
+def calculate_efficiency(dataframe):
+    df = pd.DataFrame(dataframe)
+    l_bar = sum(df['Length']*df['Probability'])
+    print("Average code length: " + str(l_bar))
+
+    entropy = sum(df['Probability']*np.log2(1/df['Probability']))
+    print("Entropy: " + str(entropy))
+
+    efficiency = round(entropy/l_bar*100, 2)
+    print("Efficiency: " + str(efficiency) + " %")
+
+    return efficiency
+
+
+def main():
+    text = input("Enter the text to encode: ").upper()
+    test = huffman_coding(text)
+    print(test)
+    calculate_efficiency(test)
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -2,54 +2,86 @@
 import tkinter as tk
 from tkinter import ttk
 
-# Modernized tkinter libraries
+# Modernized tkinter library
 import customtkinter as ctk
-from CTkTable import *
 
+import huffman_coding
 # Function when submit button is pressed
 def button_action():
-    print("Hello World!")
+    if not len(input_name.get()) == 0:
+        table_title.configure(text="Encoding Table for text " + input_name.get())
+        df = huffman_coding.huffman_coding(input_name.get())
+        calculation.configure(text="Efficiency: " + str(huffman_coding.calculate_efficiency(df)) + "%")
+        populate_tree(tree, df)
 
 # App frame
 app = ctk.CTk()
 app.geometry("720x480")
-app.title("Test")
+app.title("Huffman Encoder")
 
 # App Appearance
-ctk.set_appearance_mode("System")
+ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
 
 # App Grid Layout
 app.columnconfigure(0, weight=1, uniform='a')
 app.columnconfigure(1, weight=2, uniform='a')
-app.rowconfigure((0,1,2,3,4,5,6,7,8), weight=1, uniform='a')
+app.rowconfigure((0,1,2,3,4,5,6,7), weight=1, uniform='a')
 
 # Content
-title = ctk.CTkLabel(app, text="Input name: ", width = 200)
-title.grid(row=0, column=0)
+title = ctk.CTkLabel(app, text="Input name: ")
+title.grid(row=0, column=0, padx=40, sticky="w")
 
 input_name = tk.StringVar()
 input_field = ctk.CTkEntry(app, width=350, height=40, textvariable=input_name)
-input_field.grid(row=0, column=1, sticky='w')
+input_field.grid(row=0, column=1, sticky='we', padx=40)
 
-button = ctk.CTkButton(app, text="Generate Encoding Table", command=button_action)
+button = ctk.CTkButton(app, text="Generate Encoding Table", command=button_action, width=400)
 button.grid(row=1, column=0, columnspan=2)
 
+table_title = ctk.CTkLabel(app, text="")
+table_title.grid(row=2, column=0, columnspan=2, padx=40, sticky='w')
 
-# Encoding table
-huffman_table_headers = [['Symbol', 'Probability', 'Codeword', 'Length']]
-huffman_table = CTkTable(app, row=1, column=4, values=huffman_table_headers)
-#huffman_table.pack()
-test = [[1, 2, 3, 4], [1, 2, 3, 4]]
+calculation = ctk.CTkLabel(app, text="")
+calculation.grid(row=6, column=0, columnspan=2, padx=40, sticky='w')
 
 
-'''
-huffman_table = ttk.Treeview(app, columns=('symbol', 'probability', 'codeword', 'length'), show='headings' )
-huffman_table.heading('symbol', text="Symbol")
-huffman_table.heading('probability', text="Probability")
-huffman_table.heading('codeword', text="Codeword")
-huffman_table.heading('length', text="Length")
-huffman_table.pack()
-'''
+# Populate Treeview with DataFrame data
+def populate_tree(tree, dataframe):
+    # Delete all previous data
+    tree.delete(*tree.get_children()) 
+    
+    # Insert column headings
+    tree['columns'] = list(dataframe.columns)
+    for col in dataframe.columns:
+        tree.heading(col, text=col)
+        tree.column(col, minwidth=0, width=100)
+
+    # Insert data rows
+    for i, row in dataframe.iterrows():
+        tree.insert('', 'end', values=list(row))
+
+# Create a Treeview widget
+tree = ttk.Treeview(app, show='headings')
+tree.grid(row=3, column=0, rowspan=3, columnspan=3, padx=100, sticky="NSEW")
+
+# Treeview Modern dark look, taken from https://github.com/TomSchimansky/CustomTkinter/discussions/431
+style = ttk.Style()
+style.theme_use("default")
+style.configure("Treeview",
+    background="#2a2d2e",
+    foreground="white",
+    rowheight=25,
+    fieldbackground="#343638",
+    bordercolor="#343638",
+    borderwidth=0)
+style.map('Treeview', background=[('selected', '#22559b')])
+style.configure("Treeview.Heading",
+    background="#565b5e",
+    foreground="white",
+    relief="flat")
+style.map("Treeview.Heading",
+    background=[('active', '#3484F0')])
+
 # Run app
 app.mainloop()
